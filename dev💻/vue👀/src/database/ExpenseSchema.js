12bea6b60b22ee13336400df8ba31a8fe2451967
primopietro <Pietro.hyde@gmail.com>
2018-10-02 18:16:01 +0200

    added expense schema

---
+++ dev💻/vue👀/src/database/ExpenseSchema.js
@@ -0,0 +1,23 @@
+const expenseSchema = {
+    title: 'Expenses schema',
+    description: 'Describes a transaction by its name and its value. The type is optional.',
+    version: 0,
+    type: 'object',
+    properties: {
+        name: {
+            type: 'string',
+            primary: true
+        },
+        type: {
+            name: 'string'
+        },
+        value: {
+            type: 'number',
+            min: 0,
+            max: 100000
+        }
+    },
+    required: ['value']
+};
+
+export default expenseSchema;
